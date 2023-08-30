c74bbefbc073e72bbca37ba1d90e47f5fc53ae88
KUMA93 <kyuh2002@gmail.com>
2023-08-30 21:13:09 +0900

    kuma

---
+++ KUMA/wk.3/롤케이크자르기.java
@@ -0,0 +1,40 @@
+package src.wk3;
+
+import java.util.*;
+
+public class 롤케이크자르기 {
+    public static int solution(int[] topping) {
+        int answer = 0;
+        int size = topping.length;
+
+        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // 해시맵에 모든 토핑 개수 저장
+        for (int i = 0; i < size; i++) {
+            if (map.get(topping[i]) == null) {
+                map.put(topping[i], 1);
+            } else{
+                map.put(topping[i], map.get(topping[i]) + 1);
+            }
+        }
+
+        // 왼쪽부분 하나씩 카운트
+        for (int i = 0; i < size; i++) {
+            set.add(topping[i]);
+            map.put(topping[i], map.get(topping[i]) - 1);
+            if (map.get(topping[i]) == 0){
+                map.remove(topping[i]);
+            }
+            if (set.size() == map.size()) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {1, 2, 1, 3, 1, 4, 1, 2}));
+    }
+}
